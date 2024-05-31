ea7f15c74033c589eee883f0d31c19badac5e8c9
morellajnqa <more82@gmail.com>
2024-05-30 22:15:14 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = 'https://cnt-c2464a89-476c-4b72-94fa-8e602e791b58.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-21a9485b-efd3-4974-bb1b-21110c8ce71c.containerhub.tripleten-services.com'
 CREATE_USER_PATH = '/api/v1/users/' # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = '/api/v1/kits'# Almacena la ruta para crear un kit en esta variable
