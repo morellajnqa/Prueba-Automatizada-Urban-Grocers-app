c2d61d6a677913601393669d7cd16ef9baffdad1
JOSE LUNA <jose.luna@icarus.cl>
2024-05-30 22:06:22 -0400

    actualiza url

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = 'https://cnt-c2464a89-476c-4b72-94fa-8e602e791b58.containerhub.tripleten-services.com'
+URL_SERVICE = 'https://cnt-d4cd3cde-e930-4a2a-8648-8bf29fafe8df.containerhub.tripleten-services.com'
 CREATE_USER_PATH = '/api/v1/users/' # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = '/api/v1/kits'# Almacena la ruta para crear un kit en esta variable
